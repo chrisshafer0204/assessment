14047d63f8bc7efd9ab71557ca2579a6176ce879
chrisshafer0204 <122321876+chrisshafer0204@users.noreply.github.com>
2023-01-15 17:17:50 -0500

    Fixed edittext design

--- app/src/main/java/onesky/assessment/feature_country/presentation/country_list/CountryListScreen.kt
+++ app/src/main/java/onesky/assessment/feature_country/presentation/country_list/CountryListScreen.kt
@@ -1,10 +1,7 @@
 package onesky.assessment.feature_country.presentation.country_list
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
@@ -18,6 +15,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import onesky.assessment.R
 import onesky.assessment.feature_country.presentation.components.SubTitle
@@ -25,6 +23,8 @@ import onesky.assessment.feature_country.presentation.components.TopBar
 import onesky.assessment.feature_country.presentation.country_list.bottomnavigation.BottomNavigationBar
 import onesky.assessment.feature_country.presentation.country_list.bottomnavigation.BottomNavItem
 import onesky.assessment.feature_country.presentation.utils.Screen
+import onesky.assessment.ui.theme.AssessmentTheme
+import onesky.assessment.ui.theme.SearchBackground
 
 @Composable
 fun CountryListScreen(
@@ -34,7 +34,7 @@ fun CountryListScreen(
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
     val countryList = stringArrayResource(id = R.array.countries_array)
-    var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+    var textValue by rememberSaveable(stateSaver = TextFieldValue.Saver) {
         mutableStateOf(TextFieldValue(""))
     }
 
@@ -53,31 +53,46 @@ fun CountryListScreen(
                 ),
             )
         },
-    ) {paddingValues->
+    ) { paddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(Color.White)
-                .padding(paddingValues),
+                .padding(paddingValues)
+                .padding(top = 20.dp, start = 16.dp, end = 16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            // Add Title
-            SubTitle(subTitle = stringResource(id = R.string.str_choose_country))
             //Place the composable SearchView wherever is needed
             TextField(
-                value = text,
-                onValueChange = { it-> text = it },
-                label = { Text("Enter country name") }
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(54.dp),
+                value = textValue,
+                onValueChange = {
+                    if (!it.text.contains("\n"))
+                        textValue = it
+                },
+                label = { Text(stringResource(id = R.string.str_search_country)) },
+                maxLines = 1,
+                singleLine = true,
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = SearchBackground,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    focusedLabelColor = Color.DarkGray,
+                    cursorColor = Color.LightGray
+                ),
             )
 
-            val searchKey = text.text
+            val searchKey = textValue.text
             // Set Country List
             CountryList(navController = navController, countryList = countryList, searchKey)
-
         }
     }
 }
+
 @Composable
 fun CountryList(navController: NavController, countryList: Array<String>, searchKey: String) {
     LazyColumn(modifier = Modifier.fillMaxSize()) {
@@ -90,7 +105,7 @@ fun CountryList(navController: NavController, countryList: Array<String>, search
                     popUpTo("countries")
                 }
             })
-            
+
         }
 
     }
--- app/src/main/java/onesky/assessment/ui/theme/Color.kt
+++ app/src/main/java/onesky/assessment/ui/theme/Color.kt
@@ -4,3 +4,4 @@ import androidx.compose.ui.graphics.Color
 
 val DarkGray = Color(0xFF202020)
 val LightBlue = Color(0xFFD7E8DE)
+val SearchBackground = Color(0xFFEEEEEE)
